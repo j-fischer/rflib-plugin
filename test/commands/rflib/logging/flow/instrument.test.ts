477cfe8c9473f435ee9a24aba3b6012c736de703
Johannes Fischer <fischer.jh@gmail.com>
2025-02-17 08:15:08 -0700

    build: generated new command rflib logging flow instrument

---
+++ test/commands/rflib/logging/flow/instrument.test.ts
@@ -0,0 +1,40 @@
+import { TestContext } from '@salesforce/core/testSetup';
+import { expect } from 'chai';
+import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
+import RflibLoggingFlowInstrument from '../../../../../src/commands/rflib/logging/flow/instrument.js';
+
+describe('rflib logging flow instrument', () => {
+  const $$ = new TestContext();
+  let sfCommandStubs: ReturnType<typeof stubSfCommandUx>;
+
+  beforeEach(() => {
+    sfCommandStubs = stubSfCommandUx($$.SANDBOX);
+  });
+
+  afterEach(() => {
+    $$.restore();
+  });
+
+  it('runs hello', async () => {
+    await RflibLoggingFlowInstrument.run([]);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello world');
+  });
+
+  it('runs hello with --json and no provided name', async () => {
+    const result = await RflibLoggingFlowInstrument.run([]);
+    expect(result.path).to.equal('src/commands/rflib/logging/flow/instrument.ts');
+  });
+
+  it('runs hello world --name Astro', async () => {
+    await RflibLoggingFlowInstrument.run(['--name', 'Astro']);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello Astro');
+  });
+});
